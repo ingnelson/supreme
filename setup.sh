d6e3730a6cc0d8ff09564a32ad49e56133cb5f1d
Nelson <ingnelson86@gmail.com>
2023-01-02 12:51:58 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -223,7 +223,7 @@ if [ ! -f "/etc/log-create-user.log" ]; then
 echo "Log All Account " > /etc/log-create-user.log
 fi
 history -c
-serverV=$( curl -sS https://raw.githubusercontent.com/nanotechid/supreme/aio/permission/versi  )
+serverV=$( curl -sS https://raw.githubusercontent.com/ingnelson/supreme/aio/permission/versi  )
 echo $serverV > /opt/.ver
 aureb=$(cat /home/re_otm)
 b=11
